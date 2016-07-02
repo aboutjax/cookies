6847f3a83e9a08e4415d368c8ee913b48d16c447
Jacky Lee <aboutjax1990@gmail.com>
2016-07-02 16:53:11 +1200

    Update

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var NODE_COOKIE_NAME = $(".cookie-name");
 var chocChunc = function() {
   NODE_COOKIE_NAME.html("Choc Chunk")
   NODE_COOKIE.addClass("cookie--choc-chunk")
-  NODE_COOKIE.removeClass("cookie--black-forrest")
+  NODE_COOKIE.removeClass("cookie--black-forest")
   NODE_COOKIE.removeClass("cookie--snickers")
 }
 
-var blackForrest = function() {
-  NODE_COOKIE_NAME.html("Black Forrest")
-  NODE_COOKIE.addClass("cookie--black-forrest")
+var blackForest = function() {
+  NODE_COOKIE_NAME.html("Black forest")
+  NODE_COOKIE.addClass("cookie--black-forest")
   NODE_COOKIE.removeClass("cookie--choc-chunk")
   NODE_COOKIE.removeClass("cookie--snickers")
 }
@@ -19,5 +19,5 @@ var snickers = function() {
   NODE_COOKIE_NAME.html("Snickers")
   NODE_COOKIE.addClass("cookie--snickers")
   NODE_COOKIE.removeClass("cookie--choc-chunk")
-  NODE_COOKIE.removeClass("cookie--black-forrest")
+  NODE_COOKIE.removeClass("cookie--black-forest")
 }
