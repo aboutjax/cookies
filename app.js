7eb718e3b71e1ba973a7111a79bd1767e1b8b1cf
Jacky Lee <aboutjax1990@gmail.com>
2016-07-02 12:58:27 +1200

    Update crumbs

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var NODE_COOKIE = $(".cookie");
+var NODE_COOKIE_NAME = $(".cookie-name");
 
 var chocChunc = function() {
+  NODE_COOKIE_NAME.html("Choc Chunk")
   NODE_COOKIE.addClass("cookie--choc-chunk")
   NODE_COOKIE.removeClass("cookie--black-forrest")
   NODE_COOKIE.removeClass("cookie--snickers")
 }
 
 var blackForrest = function() {
+  NODE_COOKIE_NAME.html("Black Forrest")
   NODE_COOKIE.addClass("cookie--black-forrest")
   NODE_COOKIE.removeClass("cookie--choc-chunk")
   NODE_COOKIE.removeClass("cookie--snickers")
 }
 
 var snickers = function() {
+  NODE_COOKIE_NAME.html("Snickers")
   NODE_COOKIE.addClass("cookie--snickers")
   NODE_COOKIE.removeClass("cookie--choc-chunk")
   NODE_COOKIE.removeClass("cookie--black-forrest")
